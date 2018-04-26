755470796a26a4d374a0b27d24c6af43d6a3d3ef
Lucas Hill <kingofallhills@gmail.com>
2018-04-25 17:15:38 -0700

    Map some controller buttons.

--- frontend/chip8.js
+++ frontend/chip8.js
@@ -6,24 +6,15 @@ const CANVAS_HEIGHT = 32;
 const mainCtx = initializeCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
 (async function() {
-  const cpu = await intializeEmulatorForGame('WIPEOUT');
-
-  document.addEventListener('keydown', event => {
-    const key = event.key.toLowerCase();
-    cpu.gamepad_down(key);
-  });
-
-  document.addEventListener('keyup', event => {
-    const key = event.key;
-    cpu.gamepad_up(key);
-  });
-
+  const cpu = await intializeEmulatorForGame('PONG2');
+  setupXboxController(cpu);
+  setUpKeyboardListeners(cpu);
   window.setInterval(execute_cycle.bind(this, cpu), 2);
 })();
 
 function execute_cycle(emulator) {
   const result = emulator.tick();
-  console.log('tick');
+  // console.log('tick');
   const displayState = result.get_display_state();
   updateCanvas(displayState, mainCtx, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
@@ -58,3 +49,68 @@ async function intializeEmulatorForGame(name) {
   return CPU.new(cartridge)
 }
 
+function setUpKeyboardListeners(cpu) {
+  document.addEventListener('keydown', event => {
+    const key = event.key.toLowerCase();
+    cpu.gamepad_down(key);
+  });
+
+  document.addEventListener('keyup', event => {
+    const key = event.key;
+    cpu.gamepad_up(key);
+  });
+}
+
+function setupXboxController(cpu) {
+  const gamepads = [];
+  window.addEventListener("gamepadconnected", function(e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index, e.gamepad.id,
+      e.gamepad.buttons.length, e.gamepad.axes.length);
+
+    gamepads.push(e.gamepad);
+  });
+
+  window.addEventListener("gamepaddisconnected", function(e) {
+    console.log("Gamepad disconnected from index %d: %s",
+    e.gamepad.index, e.gamepad.id);
+
+    const index = gamepads.findIndex(gamepad => gamepad.id === e.gamepad.id);
+    gamepads.slice(index, 1);
+  });
+
+  function checkGamepads() {
+    if(gamepads.length > 1) {
+      const up1 = gamepads[0].buttons[0]
+      if(up1.pressed) {
+        cpu.gamepad_down('2');
+      } else {
+        cpu.gamepad_up('2');
+      }
+
+      const down1 = gamepads[0].buttons[1]
+      if(down1.pressed) {
+        cpu.gamepad_down('q');
+      } else {
+        cpu.gamepad_up('q');
+      }
+
+      const up2 = gamepads[1].buttons[0]
+      if(up2.pressed) {
+        cpu.gamepad_down('z');
+      } else {
+        cpu.gamepad_up('z');
+      }
+
+      const down2 = gamepads[1].buttons[1]
+      if(down2.pressed) {
+        cpu.gamepad_down('x');
+      } else {
+        cpu.gamepad_up('x');
+      }
+    }
+
+  }
+
+  setInterval(checkGamepads, 200);
+}
